1bf6f22c64fa12782369545043010a52a66a5339
saeed <saeeddadgar66@gmail.com>
2019-03-09 00:46:05 +0000

    completed exercises week-1-practise

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,10 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+//It gives a number between 0 and 1 including 0 and then multiplies it by 10.
 // - How many parameters does the function take?
+// none
 // - What is the function name?
+//getNumber
 // - Where is the function called? with what parameters?
+//var result
--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,14 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
+//It takes two different strings and gives a new string.
 // - How many parameters does the function take?
+//It takes 2.
 // - What is the function name?
+//s
 // - Where is the function called? with what parameters?
+//var result
 // - What does the function return?
+//It returns codeyour
 // - How can this function be improved?
+//This function does not have an appropriate name.A better name for it can be str.
