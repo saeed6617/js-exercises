69e9e1d24b0aeba68e46c1a27aa3c606d937b74e
saeed <saeeddadgar66@gmail.com>
2019-03-30 15:49:32 +0000

    completed week-4

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,11 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+let laptop = {
+  brand: "del",
+  color: "grey",
+  size: 14,
+  ram: 64,
+  owner: "cpdeYourFuture",
+  opertatinSystem: "linux"
+};
--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -6,3 +6,34 @@ can describe with a JavaScript object
 Assign each of them to a separate variable
 
 */
+var myFlat = {
+  numberOfBedroom: 2,
+  colorsOfWalls: "white",
+  isThereATv: true,
+  size: 110,
+  isThereAMasterToilet: false
+};
+var mobilePhone = {
+  size: 7,
+  color: black,
+  isUsersFriendly: true,
+  brand: "iphone"
+};
+
+var arthur = {
+  isApproachable: true,
+  phoneNumber: "0747848272",
+  hasGlasses: true,
+  hasPet: false,
+  lookingForNewWife: false
+};
+var iran = {
+  capital: "tehran",
+  population: 70000000,
+  mainCities: ["shiraz", "esfahan", "tabriz"]
+};
+var myPreviusCar = {
+  brand: "peugeot",
+  color: "silver",
+  maxSpeed: 200
+};
